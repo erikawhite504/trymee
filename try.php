ba7c896e1f33e7ff3da65654b3d1cc8d28de5f39
erikawhite504 <71593382+erikawhite504@users.noreply.github.com>
2020-11-09 14:56:35 +0800

    Update and rename try.html to try.php

---
+++ try.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>WEATHER UPDATE</title>
+
+<script src="http://code.jquery.com/jquery-3.2.1.min.js"
+intefrity="sha256-hwg4gsxgFZhOsEEamdOVGBf13fyQuiTwlAQgxVSNgta="
+crossorigin="anonymous"></script>
+</head>
+<body>
+
+<input id="city"></input>
+<button id="getWeatherForcast">Get Weather</button>
+<div id="showWeatherForcast"></div>
+
+
+<script type="text/javascript">
+  $(document).ready(function() {
+    $("#getWeatherForcast").click(function(){
+      var city = $("#city").val();
+      var key = '8df08730e309d4e3f7164b6abd5a7735';
+
+
+      $.ajax({  
+        url: 'http://api.openweathermap.org/data/2.5/weather',
+        dataType:'json',
+        type:'GET',
+        data:{q:city, appid: key, units: 'metric'},
+
+        success: function(data){
+        	var wf='';
+        	$.each(data.weather, function(index, val){
+        		wf += '<p><br>' + data.name + "</b><img src="+ val.icon +".png></p>"+
+        		data.main.temp + '&deg;C' + '|' + val.main + "," + val.description
+        	});
+        	$("#showWeatherUpdate").html(wf)
+        }
+
+
+
+      });
+    });
+
+  });
+
+
+</script>
+
+</body>
+</html>
